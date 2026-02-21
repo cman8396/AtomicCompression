6d95b5705ae8ff8d06120f34af80035d9eaaa241
cman8396 <c-man8396@hotmail.co.uk>
2026-02-21 16:46:52 +0000

    New Atomic Shard texture

--- src/main/java/com/mcnair/atomic/utility/AtomicCreativeTabs.java
+++ src/main/java/com/mcnair/atomic/utility/AtomicCreativeTabs.java
@@ -38,20 +38,20 @@ public class AtomicCreativeTabs {
                         output.accept(AtomicBlocks.END_ATOMIC_ORE.get());
 
                         // items
-                        output.accept(AtomicItems.ATOMIC_SHARD.get());
                         output.accept(AtomicItems.RAW_SULFUR.get());
                         output.accept(AtomicItems.RAW_SALTPETER.get());
                         output.accept(AtomicItems.RAW_LEAD.get());
                         output.accept(AtomicItems.RAW_BUNGERITE.get());
                         output.accept(AtomicItems.SULFUR_DUST.get());
                         output.accept(AtomicItems.SALTPETER_DUST.get());
-                        output.accept(AtomicItems.EMPOWERED_ATOMIC_SHARD.get());
                         output.accept(AtomicItems.LEAD_INGOT.get());
                         output.accept(AtomicItems.REFINED_BUNGERITE.get());
                         output.accept(AtomicItems.BUNGERITE_ALLOY_INGOT.get());
                         output.accept(AtomicItems.LEAD_NUGGET.get());
                         output.accept(AtomicItems.REFINED_BUNGERITE_NUGGET.get());
                         output.accept(AtomicItems.BUNGERITE_ALLOY_NUGGET.get());
+                        output.accept(AtomicItems.ATOMIC_SHARD.get());
+                        output.accept(AtomicItems.EMPOWERED_ATOMIC_SHARD.get());
 
                     }).build()
     );
